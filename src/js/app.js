80d4373daccaa82d727dd68416a11c8ddab944cf
Billy Ceskavich <bceskavich@gmail.com>
2015-10-05 11:03:48 -0400

    css-patterns library + live linting

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import connectToStores from 'alt/utils/connectToStores';
 import AppStore from './stores/AppStore';
 
 @connectToStores
 export default class App extends Component {
+
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  }
 
   static getStores() {
     return [AppStore];
